15f981a21fd4ba27c8431dd34bd9dc711949c1de
kimromi <kimromi4@gmail.com>
2018-10-11 23:48:54 +0900

    screenshot with puppeteer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const puppeteer = require('puppeteer')
 
 const app = new Koa()
 const router = new Router()
@@ -7,13 +8,24 @@ const router = new Router()
 router.get('/', (ctx, next) => {
   ctx.body = 'Hello KShot!'
 })
-router.get('/shot', (ctx, next) => {
+router.get('/shot', async (ctx, next) => {
   const url = ctx.request.query.url
   if (typeof url === 'undefined') {
     ctx.body = 'please set parameter `url`.'
     return
   }
-  ctx.body = ctx.request.query.url
+
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  try {
+    await page.goto(url, { waitUntil: 'load' })
+    await page.screenshot({ path: '/tmp/shot.png', fullPage: true })
+    ctx.body = 'OK!'
+  } catch (e) {
+    ctx.body = 'Error!'
+  } finally {
+    await browser.close()
+  }
 })
 
 app
